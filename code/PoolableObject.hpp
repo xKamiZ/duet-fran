f810143e6fb5b7c1d0a02d1512e5f8193172b611
xKamiZ <officialdarkshadows@gmail.com>
2023-01-22 23:17:38 +0100

    Update 0.2.2.1
    Iniciada implementación Poolable Object

--- code/PoolableObject.hpp
+++ code/PoolableObject.hpp
@@ -1,6 +1,7 @@
-//
-// Created by franc on 22/01/2023.
-//
+/*
+ * POOLABLE OBJECT CLASS
+ * AUTHOR: Fran Caamaño Martínez
+ */
 
 #ifndef BASICS_PROJECT_TEMPLATE_POOLABLEOBJECT_HPP
 #define BASICS_PROJECT_TEMPLATE_POOLABLEOBJECT_HPP
@@ -8,6 +9,20 @@
 namespace DuetClone {
 
     class PoolableObject {
+
+    public:
+
+        PoolableObject()
+        {
+            _inUse = true;
+        }
+
+        bool InUse() { return _inUse; };
+        void SetInUse(bool newState) {_inUse = newState; }
+
+    private:
+
+        bool _inUse;
 
     };
 
